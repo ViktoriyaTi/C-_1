4a051cbdb4951ee82b3465c8068ceb4c7e5620bc
ViktoriyaTim <ViktoriyaTimon@gmail.com>
2022-08-03 15:32:13 +0300

    New Random

--- example003_Sum/Program.cs
+++ example003_Sum/Program.cs
@@ -1,6 +1,8 @@
 ﻿// Сложение двух чисел
-int numberA = 3;
-int numberB = 5;
+int numberA = new Random().Next(1,10); //1,2,3,4,5,6,7,8,9
+int numberB = new Random().Next(1,10);
 int sum = numberA + numberB;
+Console.WriteLine (numberA);
+Console.WriteLine (numberB);
 Console.Write("Сумма= ");
 Console.Write(sum);
