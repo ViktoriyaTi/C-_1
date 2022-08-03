379042c7f88894efc0513c6079f89249dc0a41f5
ViktoriyaTim <ViktoriyaTimon@gmail.com>
2022-08-03 15:50:57 +0300

    Example005_IF_Else

---
+++ example005_IF_Else/Program.cs
@@ -0,0 +1,13 @@
+﻿// Особенное приветсвие 
+Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("УРА, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
