aea315aeacc1e864277f400d2d5a89d9cd007711
ViktoriyaTim <ViktoriyaTimon@gmail.com>
2022-08-08 19:36:47 +0300

    Example009

---
+++ example009/Program.cs
@@ -0,0 +1,17 @@
+﻿int Max(int arg1, int arg2,int arg3)
+{
+int rezult = arg1;
+if (arg2 > rezult) rezult = arg2;
+if (arg3> rezult) rezult = arg3;
+return rezult;
+}
+
+int[] array = {1,21,3,4,5,6,7,8,9};
+
+
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+Max(array[3], array[4], array[5]),
+Max(array[6], array[7], array[8]));
+Console.WriteLine(max);
