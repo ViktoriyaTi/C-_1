d05e39bbc14164f8ea59e72c7b7c5733f8e42f7c
ViktoriyaTim <ViktoriyaTimon@gmail.com>
2022-08-08 19:37:12 +0300

    Example010

---
+++ example010/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1,2,1,141,5,6,7,14,14,14};
+
+int n = array.Length;
+int find = 14;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
